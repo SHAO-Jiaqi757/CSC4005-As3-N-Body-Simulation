264fea54b42499bf836ce8e879f2e162cc366fbb
Luuvy <119010256@link.cuhk.edu.cn>
2021-11-07 10:05:10 +0800

    init

---
+++ build/CMakeFiles/core.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/imgui/backends/imgui_impl_opengl2.cpp" "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/build/CMakeFiles/core.dir/imgui/backends/imgui_impl_opengl2.cpp.o"
+  "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/imgui/backends/imgui_impl_sdl.cpp" "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/build/CMakeFiles/core.dir/imgui/backends/imgui_impl_sdl.cpp.o"
+  "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/imgui/imgui.cpp" "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/build/CMakeFiles/core.dir/imgui/imgui.cpp.o"
+  "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/imgui/imgui_demo.cpp" "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/build/CMakeFiles/core.dir/imgui/imgui_demo.cpp.o"
+  "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/imgui/imgui_draw.cpp" "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/build/CMakeFiles/core.dir/imgui/imgui_draw.cpp.o"
+  "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/imgui/imgui_tables.cpp" "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/build/CMakeFiles/core.dir/imgui/imgui_tables.cpp.o"
+  "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/imgui/imgui_widgets.cpp" "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/build/CMakeFiles/core.dir/imgui/imgui_widgets.cpp.o"
+  "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/imgui/misc/cpp/imgui_stdlib.cpp" "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/build/CMakeFiles/core.dir/imgui/misc/cpp/imgui_stdlib.cpp.o"
+  "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/imgui/misc/freetype/imgui_freetype.cpp" "/Users/jiaqishao/OneDrive - CUHK-Shenzhen/CSC4005/code/HW/As3_119010256/build/CMakeFiles/core.dir/imgui/misc/freetype/imgui_freetype.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ImDrawIdx=unsigned"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../imgui"
+  "../imgui/backends"
+  "/usr/local/Cellar/open-mpi/4.1.1_2/include"
+  "/usr/local/Cellar/sdl2/2.0.16/include/SDL2"
+  "/Library/Frameworks/Mono.framework/Headers"
+  "/usr/local/include/freetype2"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/System/Library/Frameworks/OpenGL.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
